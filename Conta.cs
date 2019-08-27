e32afd28c620094f96ad222eeeee27d546dc0245
Pedro Dantas | Stone <pedro.dantas@stone.com.br>
2019-08-27 18:43:26 -0300

    Fist Commit

---
+++ Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudo
+{
+     
+    public class Conta
+    {
+        //atributos
+        public float Saldo { get; set; }
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+
+        //Comportamento da classe ou métodos
+        public bool Saca(float valor)
+        {
+            if (this.Saldo >= valor)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public void Deposita(float valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Transfere(float valor, Conta destino)
+        {
+            if (this.Saca(valor))
+            {
+                destino.Deposita(valor);
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //instanciação da classe conta 
+            Conta contaPedro = new Conta();
+            contaPedro.Titular = "pedro";
+            contaPedro.Numero = 1;
+            contaPedro.Saldo = 1000;
+
+            Conta contaEduardo = new Conta();
+            contaEduardo.Titular = "eduardo";
+            contaEduardo.Numero = 2;
+            contaEduardo.Saldo = 500;
+
+            contaPedro.Saldo += 200.0f;
+            contaEduardo.Saldo -= 300.0f;
+
+            MessageBox.Show("Pedro = " + contaPedro.Saldo.ToString());
+            MessageBox.Show("Eduardo = " + contaEduardo.Saldo.ToString());
+
+            /*contaPedro.Deposita(700.0f);
+            MessageBox.Show("A depósito foi de: " + contaPedro.Saldo.ToString());
+            contaPedro.Saca(50.0f);
+            MessageBox.Show("Saldo" + contaPedro.Saldo.ToString());*/
+
+
+            /*bool deuCerto = contaPedro.Saca(500.0f);
+
+            if (deuCerto)
+            {
+                MessageBox.Show("Saque realizado com sucesso: " + contaPedro.Saldo.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Saldo insuficiente");
+            }*/
+
+        }
+    }
+}
